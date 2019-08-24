f8363fa9c8c74ea520332e0e85bdc5896863bbfd
Ivan Deiko <IvanDeiko97@gmail.com>
2019-08-24 17:14:58 +0300

    Updates

---
+++ precache-manifest.1e57f3708bb155d02cbab60b56681bcb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74c6eb4bdb54cbc148744b89d7cb3417",
+    "url": "/block1/index.html"
+  },
+  {
+    "revision": "287517fed28fb8eb354a",
+    "url": "/block1/static/css/main.250bc733.chunk.css"
+  },
+  {
+    "revision": "f5b11bacc36c87c0431e",
+    "url": "/block1/static/js/2.681bc579.chunk.js"
+  },
+  {
+    "revision": "287517fed28fb8eb354a",
+    "url": "/block1/static/js/main.c74d62b6.chunk.js"
+  },
+  {
+    "revision": "f599e418aa86df0d5507",
+    "url": "/block1/static/js/runtime~main.96979bad.js"
+  },
+  {
+    "revision": "4abbdfd32c05fb55317defec2d45dc25",
+    "url": "/block1/static/media/AudiType-Bold_03.4abbdfd3.woff"
+  },
+  {
+    "revision": "6890e6fade5253987ed3a29508fbce70",
+    "url": "/block1/static/media/AudiTypeScreen-Light.6890e6fa.woff"
+  },
+  {
+    "revision": "07756115b8560cc62046d560bb5de1e3",
+    "url": "/block1/static/media/car.07756115.png"
+  }
+]);
